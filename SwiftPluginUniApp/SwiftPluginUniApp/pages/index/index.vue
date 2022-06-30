a38df2209584818280b3e8a445c48f27a1650a15
liang <liangliangaichirou@163.com>
2022-06-30 21:44:15 +0800

    init

---
+++ SwiftPluginUniApp/SwiftPluginUniApp/pages/index/index.vue
@@ -0,0 +1,67 @@
+<template>
+	<view>
+		<view style="padding: 20px;">
+			<input v-model="command" placeholder="输入命令" />
+			<button style="margin-top: 20px;" type="primary" @click="sendCommand">发送</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	// 首先需要通过 uni.requireNativePlugin("ModuleName") 获取 module 
+	var plugin = uni.requireNativePlugin("SwiftPlugin-SwiftEntrance")
+
+	export default {
+		data() {
+			return {
+				command: '',
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			sendCommand() {
+				console.log(this.command);
+				plugin.sendCommand({
+						'name': 'uni-app',
+						'command': this.command
+					},
+					(result) => {
+						uni.showToast({
+							title: result,
+							icon: "none"
+						})
+					})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ SwiftPluginUniApp/SwiftPluginUniApp/unpackage/dist/dev/app-plus/app-service.js
@@ -0,0 +1,156 @@
+if (typeof Promise !== "undefined" && !Promise.prototype.finally) {
+  Promise.prototype.finally = function(callback) {
+    const promise = this.constructor;
+    return this.then((value) => promise.resolve(callback()).then(() => value), (reason) => promise.resolve(callback()).then(() => {
+      throw reason;
+    }));
+  };
+}
+;
+if (typeof uni !== "undefined" && uni && uni.requireGlobal) {
+  const global = uni.requireGlobal();
+  ArrayBuffer = global.ArrayBuffer;
+  Int8Array = global.Int8Array;
+  Uint8Array = global.Uint8Array;
+  Uint8ClampedArray = global.Uint8ClampedArray;
+  Int16Array = global.Int16Array;
+  Uint16Array = global.Uint16Array;
+  Int32Array = global.Int32Array;
+  Uint32Array = global.Uint32Array;
+  Float32Array = global.Float32Array;
+  Float64Array = global.Float64Array;
+  BigInt64Array = global.BigInt64Array;
+  BigUint64Array = global.BigUint64Array;
+}
+;
+if (uni.restoreGlobal) {
+  uni.restoreGlobal(Vue, weex, plus, setTimeout, clearTimeout, setInterval, clearInterval);
+}
+(function(vue, shared) {
+  "use strict";
+  function isDebugMode() {
+    return typeof __channelId__ === "string" && __channelId__;
+  }
+  function jsonStringifyReplacer(k, p) {
+    switch (shared.toRawType(p)) {
+      case "Function":
+        return "function() { [native code] }";
+      default:
+        return p;
+    }
+  }
+  function normalizeLog(type, filename, args) {
+    if (isDebugMode()) {
+      args.push(filename.replace("at ", "uni-app:///"));
+      return console[type].apply(console, args);
+    }
+    const msgs = args.map(function(v) {
+      const type2 = shared.toTypeString(v).toLowerCase();
+      if (["[object object]", "[object array]", "[object module]"].indexOf(type2) !== -1) {
+        try {
+          v = "---BEGIN:JSON---" + JSON.stringify(v, jsonStringifyReplacer) + "---END:JSON---";
+        } catch (e) {
+          v = type2;
+        }
+      } else {
+        if (v === null) {
+          v = "---NULL---";
+        } else if (v === void 0) {
+          v = "---UNDEFINED---";
+        } else {
+          const vType = shared.toRawType(v).toUpperCase();
+          if (vType === "NUMBER" || vType === "BOOLEAN") {
+            v = "---BEGIN:" + vType + "---" + v + "---END:" + vType + "---";
+          } else {
+            v = String(v);
+          }
+        }
+      }
+      return v;
+    });
+    return msgs.join("---COMMA---") + " " + filename;
+  }
+  function formatAppLog(type, filename, ...args) {
+    const res = normalizeLog(type, filename, args);
+    res && console[type](res);
+  }
+  function requireNativePlugin(name) {
+    return weex.requireModule(name);
+  }
+  var _export_sfc = (sfc, props) => {
+    const target = sfc.__vccOpts || sfc;
+    for (const [key, val] of props) {
+      target[key] = val;
+    }
+    return target;
+  };
+  var plugin = requireNativePlugin("SwiftPlugin-SwiftEntrance");
+  const _sfc_main$1 = {
+    data() {
+      return {
+        command: ""
+      };
+    },
+    onLoad() {
+    },
+    methods: {
+      sendCommand() {
+        formatAppLog("log", "at pages/index/index.vue:25", this.command);
+        plugin.sendCommand({
+          "name": "uni-app",
+          "command": this.command
+        }, (result) => {
+          uni.showToast({
+            title: result,
+            icon: "none"
+          });
+        });
+      }
+    }
+  };
+  function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+    return vue.openBlock(), vue.createElementBlock("view", null, [
+      vue.createElementVNode("view", { style: { "padding": "20px" } }, [
+        vue.withDirectives(vue.createElementVNode("input", {
+          "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => $data.command = $event),
+          placeholder: "\u8F93\u5165\u547D\u4EE4"
+        }, null, 512), [
+          [vue.vModelText, $data.command]
+        ]),
+        vue.createElementVNode("button", {
+          style: { "margin-top": "20px" },
+          type: "primary",
+          onClick: _cache[1] || (_cache[1] = (...args) => $options.sendCommand && $options.sendCommand(...args))
+        }, "\u53D1\u9001")
+      ])
+    ]);
+  }
+  var PagesIndexIndex = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render], ["__file", "/Users/liang/Desktop/SwiftPluginUniApp/SwiftPluginUniApp/pages/index/index.vue"]]);
+  __definePage("pages/index/index", PagesIndexIndex);
+  const _sfc_main = {
+    onLaunch: function() {
+      formatAppLog("log", "at App.vue:4", "App Launch");
+    },
+    onShow: function() {
+      formatAppLog("log", "at App.vue:7", "App Show");
+    },
+    onHide: function() {
+      formatAppLog("log", "at App.vue:10", "App Hide");
+    }
+  };
+  var App = /* @__PURE__ */ _export_sfc(_sfc_main, [["__file", "/Users/liang/Desktop/SwiftPluginUniApp/SwiftPluginUniApp/App.vue"]]);
+  function createApp() {
+    const app = vue.createVueApp(App);
+    return {
+      app
+    };
+  }
+  const { app: __app__, Vuex: __Vuex__, Pinia: __Pinia__ } = createApp();
+  uni.Vuex = __Vuex__;
+  uni.Pinia = __Pinia__;
+  __app__.provide("__globalStyles", __uniConfig.styles);
+  __app__._component.mpType = "app";
+  __app__._component.render = () => {
+  };
+  __app__.mount("#app");
+})(Vue, uni.VueShared);
